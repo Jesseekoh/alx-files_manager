9fa289967e689e63f264b25febcff5f3e31c808f
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-05-11 10:25:35 +0100

    Add environment variable loader utility

--- utils/db.js
+++ utils/db.js
@@ -1,11 +1,12 @@
-import 'dotenv/config';
+// import 'dotenv/config';
 import { MongoClient } from 'mongodb';
-
+import envLoader from './env_loader';
 /**
  *
  */
 class DBClient {
   constructor() {
+    envLoader();
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
