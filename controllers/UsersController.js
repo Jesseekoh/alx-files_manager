68d35001cebe9a701a1ec7d232c93e21da43d4e4
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2024-05-11 10:43:15 +0100

    Update UsersController to return 201 status code for successful user creation

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -17,12 +17,12 @@ export default class UsersController {
     const user = await dbClient.client.db().collection('users').findOne({ email });
 
     if (user) {
-      res.status(400).send({ error: 'Already exist' });
+      res.status(400).json({ error: 'Already exist' });
     } else {
       const insertDetails = await dbClient.client.db().collection('users').insertOne({ email, password: sha1(password) });
       console.log(insertDetails);
       const userId = insertDetails.insertedId.toString();
-      res.status(200).json({ id: userId, email });
+      res.status(201).json({ id: userId, email });
     }
   }
 }
